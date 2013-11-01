e8802a3aa2d49a21c6684af5403693fb48ee054c
Mark Adams <mark@thoughtbot.com>
2013-11-01 11:31:14 -0700

    Initial Commit

---
+++ Tipper/TPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAppDelegate.h
+//  Tipper
+//
+//  Created by Mark Adams on 11/1/13.
+//  Copyright (c) 2013 Mark Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tipper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tipper
+//
+//  Created by Mark Adams on 11/1/13.
+//  Copyright (c) 2013 Mark Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+    }
+}
